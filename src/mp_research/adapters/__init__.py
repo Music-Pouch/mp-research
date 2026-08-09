4143996ed50978c3843c50cfb49447e30c4c4268
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2026-08-09 02:39:28 -0500

    feat: export adapters

---
+++ src/mp_research/adapters/__init__.py
@@ -0,0 +1,5 @@
+from mp_research.adapters.gpt_researcher import GPTResearcherAdapter
+from mp_research.adapters.open_deep_research import OpenDeepResearchAdapter
+from mp_research.adapters.storm import StormAdapter
+
+__all__ = ["GPTResearcherAdapter", "OpenDeepResearchAdapter", "StormAdapter"]
